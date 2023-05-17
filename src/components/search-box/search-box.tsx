f6f99616e038f3ef60c3a3dccbab04266d098bc1
Madhurima <Madhurima@mcmadhurima7@gmail.com>
2023-05-18 04:29:02 +0530

    feat: Added searchbox component

--- src/components/search-box/search-box.tsx
+++ src/components/search-box/search-box.tsx
@@ -1,19 +1,82 @@
-import { Component, Host, h } from "@stencil/core";
+import { Component, Host, State, h } from "@stencil/core";
+import { Responsee } from "../../utils/Response.interface";
 
 @Component({
     tag: 'ti-hack-search-box',
     styleUrl: 'search-box.scss',
-    shadow: true
+    shadow: false
 })
 export class SearchBox{
-    componentWillLoad() {
-        console.log('Will load');
+
+    private debounceIntervalId: any = null;
+    private titleVal:string;
+
+    @State() searchResults: Responsee[] = {} as Responsee[];
+
+    /**
+     * On Input og a key in textbox, this event fires to provide
+     * suggestions for products. Currently shows results of dummy response
+     * in list format
+     * @param _ev event
+     */
+    private _callApi(_ev: InputEvent) {
+        // this.searchResults = {} as Responsee[];
+        const x = document.getElementById('searchInput') as HTMLInputElement;
+        if(x.value !==this.titleVal) {
+            this.searchResults = [];
+        }
+        this.titleVal = x.value;
+        const interval = setInterval(async () => {
+            clearInterval(interval);
+            if(interval === this.debounceIntervalId) {
+                this.debounceIntervalId=null;
+                this.searchResults=await this._fetchResult(this.titleVal);
+            }
+        }, 750);
+        this.debounceIntervalId = interval;
+    }
+
+    /**
+     * Using dummy university api http://universities.hipolabs.com/search?country=India
+     * to demonstrate the search component api call.
+     * To be substituted by NLP product suggestor API
+     * @param key Searh key
+     */
+    private _fetchResult(key: string): Promise<Responsee[]> {
+        return fetch(`http://universities.hipolabs.com/search?country=${key}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }            
+            return response.json();
+        })
+    
     }
 
     render() {
         return(
             <Host>
-                <h1>Hi</h1>
+                <div class="search-container">
+                    <input 
+                        id="searchInput"
+                        type="text" 
+                        placeholder="Search" 
+                        onInput={ev => this._callApi(ev)}
+                    />
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </div>
+                <div class="results" id="result">
+                    <ul>
+                        
+                            {Array.from(this.searchResults).map((item:any={})=>
+                            <li>
+                              {item.id} {item.name}
+                             </li>
+                             )}
+                            </ul>
+                        
+                    
+                </div>
             </Host>
         );
     }
---
+++ src/utils/Response.interface.ts
@@ -0,0 +1,8 @@
+export interface Responsee {
+    domains: string[],
+    country: string,
+    alpha_two_code: string,
+    state: string,
+    web_pages: string[],
+    name: string
+}
